4738d0c7bcccad00072d47cfd53da364c4eec954
shubhamnik99 <shubhamnikam2410@gmail.com>
2025-06-27 12:08:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. ap-south-1_932RwMO36
+        userPoolClientId: '', // e.g. 378p8t0sd57fsjdm2obdg9evs6
+        region: '' // e.g. ap-south-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
